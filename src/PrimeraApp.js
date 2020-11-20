052fdcd256e01b1ec402e833054d6f6a757bc87f
unknown <lucusapp@gmail.com>
2020-11-20 14:13:35 +0100

    empezando ...

---
+++ src/PrimeraApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Funtional Components
+
+const PrimeraApp = () =>{
+
+    return (
+
+    <>
+    <h1>Hola Mundo</h1>
+    <p>Mi primera app</p>
+    </>
+    )
+}
+
+
+export default PrimeraApp
